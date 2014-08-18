ebebc53a960cfacdf23989252889b6976e93e816
lhmouse <lh_mouse@126.com>
2014-08-18 20:18:49 +0800

    Replaced -std=c++03 with -std=c++98 to settle for g++ 4.4.

--- reconfig_debug.sh
+++ reconfig_debug.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 autoreconf -if
-CPPFLAGS='-D_GLIBCXX_DEBUG' CFLAGS='-O0 -g -std=c99' CXXFLAGS='-O0 -g -std=c++03'	\
+CPPFLAGS='-D_GLIBCXX_DEBUG' CFLAGS='-O0 -g -std=c99' CXXFLAGS='-O0 -g -std=c++98'	\
 	./configure --enable-shared --disable-static
--- reconfig_release.sh
+++ reconfig_release.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 autoreconf -if
-CPPFLAGS='-DNDEBUG' CFLAGS='-O3 -g -std=c99' CXXFLAGS='-O3 -g -std=c++03'	\
+CPPFLAGS='-DNDEBUG' CFLAGS='-O3 -g -std=c99' CXXFLAGS='-O3 -g -std=c++98'	\
 	./configure --enable-shared --disable-static
