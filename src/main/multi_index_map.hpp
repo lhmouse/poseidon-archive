03a466b50c06f8d890a4ec20a43cab1415f75575
lhmouse <lh_mouse@126.com>
2014-08-26 12:03:18 +0800

    Added MultiIndexMap.

---
+++ src/main/multi_index_map.hpp
@@ -0,0 +1,203 @@
+#ifndef POSEIDON_MULTI_INDEX_MAP_HPP_
+#define POSEIDON_MULTI_INDEX_MAP_HPP_
+
+#include <boost/multi_index_container.hpp>
+#include <boost/multi_index/member.hpp>
+#include <boost/multi_index/ordered_index.hpp>
+
+#define MULTI_INDEX_MAP(class_name_, value_type_, ...)	\
+	class class_name_ {	\
+	public:	\
+		typedef value_type_ value_type;	\
+		\
+		typedef ::boost::multi_index::multi_index_container<	\
+			value_type,	\
+			::boost::multi_index::indexed_by<__VA_ARGS__>	\
+		> delegate_container;	\
+		\
+	private:	\
+		template<unsigned long IndexId>	\
+		struct KeyModifier {	\
+			typedef typename delegate_container::nth_index<IndexId>::type::key_type	\
+				key_type;	\
+			\
+			const key_type m_key;	\
+			\
+			explicit KeyModifier(key_type key)	\
+				: m_key(key)	\
+			{	\
+			}	\
+			\
+			void operator()(key_type &old){	\
+				old = m_key;	\
+			}	\
+		};	\
+		\
+	private:	\
+		delegate_container m_delegate;	\
+		\
+	public:	\
+		bool empty() const {	\
+			return m_delegate.empty();	\
+		}	\
+		std::size_t size(){	\
+			return m_delegate.size();	\
+		}	\
+		void clear(){	\
+			m_delegate.clear();	\
+		}	\
+		\
+		template<unsigned long IndexId>  \
+		const typename delegate_container::nth_index<IndexId>::type &	\
+			getIndex() const	\
+		{	\
+			return m_delegate.get<IndexId>();	\
+		}	\
+		template<unsigned long IndexId>  \
+		typename delegate_container::nth_index<IndexId>::type &	\
+			getIndex()	\
+		{	\
+			return m_delegate.get<IndexId>();	\
+		}	\
+		\
+		std::pair<delegate_container::iterator, bool>	\
+			insert(const value_type &val)	\
+		{	\
+			return m_delegate.insert(val);	\
+		}	\
+		\
+		template<unsigned long IndexId>	\
+		std::pair<typename delegate_container::nth_index<IndexId>::type::iterator, bool>	\
+			insert(typename delegate_container::nth_index<IndexId>::type::iterator hint,	\
+				const value_type &val)	\
+		{	\
+			return getIndex<IndexId>().insert(hint, val);	\
+		}	\
+		\
+		template<unsigned IndexId>  \
+		void erase(typename delegate_container::nth_index<IndexId>::type::iterator pos){	\
+			getIndex<IndexId>().erase(pos);	\
+		}	\
+		\
+		template<unsigned long IndexId> \
+		bool replace(typename delegate_container::nth_index<IndexId>::type::iterator pos,	\
+			const value_type &val)  \
+		{	\
+			return getIndex<IndexId>().replace(pos, val);	\
+		}	\
+		template<unsigned long IndexId> \
+		bool setKey(typename delegate_container::nth_index<IndexId>::type::iterator pos,   \
+			const typename delegate_container::nth_index<IndexId>::type::key_type &key)	\
+		{	\
+			return getIndex<IndexId>().modify_key(	\
+				pos,	\
+				KeyModifier<IndexId>(key),	\
+				KeyModifier<IndexId>(	\
+					typename delegate_container::nth_index<IndexId>::type::key_from_value()(*pos)	\
+				)	\
+			);	\
+		}	\
+		\
+		template<unsigned IndexId>  \
+		typename delegate_container::nth_index<IndexId>::type::const_iterator   \
+			begin() const	\
+		{	\
+			return getIndex<IndexId>().begin();	\
+		}	\
+		template<unsigned IndexId>  \
+		typename delegate_container::nth_index<IndexId>::type::iterator   \
+			begin()	\
+		{	\
+			return getIndex<IndexId>().begin();	\
+		}	\
+		\
+		template<unsigned IndexId>  \
+		typename delegate_container::nth_index<IndexId>::type::const_iterator   \
+			end() const	\
+		{	\
+			return getIndex<IndexId>().end();	\
+		}	\
+		template<unsigned IndexId>  \
+		typename delegate_container::nth_index<IndexId>::type::iterator   \
+			end()	\
+		{	\
+			return getIndex<IndexId>().end();	\
+		}	\
+		\
+		template<unsigned IndexId>  \
+		typename delegate_container::nth_index<IndexId>::type::const_iterator	\
+			find(const typename delegate_container::nth_index<IndexId>::type::key_type &key) const	\
+		{	\
+			return getIndex<IndexId>().find(key);	\
+		}	\
+		template<unsigned IndexId>  \
+		typename delegate_container::nth_index<IndexId>::type::iterator	\
+			find(const typename delegate_container::nth_index<IndexId>::type::key_type &key)	\
+		{	\
+			return getIndex<IndexId>().find(key);	\
+		}	\
+		\
+		template<unsigned IndexId>  \
+		typename delegate_container::nth_index<IndexId>::type::const_iterator	\
+			lowerBound(const typename delegate_container::nth_index<IndexId>::type::key_type &key) const	\
+		{	\
+			return getIndex<IndexId>().lower_bound(key);	\
+		}	\
+		template<unsigned IndexId>  \
+		typename delegate_container::nth_index<IndexId>::type::iterator	\
+			lowerBound(const typename delegate_container::nth_index<IndexId>::type::key_type &key)	\
+		{	\
+			return getIndex<IndexId>().lower_bound(key);	\
+		}	\
+		\
+		template<unsigned IndexId>  \
+		typename delegate_container::nth_index<IndexId>::type::const_iterator	\
+			upperBound(const typename delegate_container::nth_index<IndexId>::type::key_type &key) const	\
+		{	\
+			return getIndex<IndexId>().upper_bound(key);	\
+		}	\
+		template<unsigned IndexId>  \
+		typename delegate_container::nth_index<IndexId>::type::iterator	\
+			upperBound(const typename delegate_container::nth_index<IndexId>::type::key_type &key)	\
+		{	\
+			return getIndex<IndexId>().upper_bound(key);	\
+		}	\
+		\
+		template<unsigned IndexId>  \
+		std::pair<typename delegate_container::nth_index<IndexId>::type::const_iterator,	\
+			typename delegate_container::nth_index<IndexId>::type::const_iterator	\
+		>	\
+			equalRange(const typename delegate_container::nth_index<IndexId>::type::key_type &key) const	\
+		{	\
+			return getIndex<IndexId>().equal_range(key);	\
+		}	\
+		template<unsigned IndexId>  \
+		std::pair<typename delegate_container::nth_index<IndexId>::type::iterator,	\
+			typename delegate_container::nth_index<IndexId>::type::iterator	\
+		>	\
+			equalRange(const typename delegate_container::nth_index<IndexId>::type::key_type &key)	\
+		{	\
+			return getIndex<IndexId>().equal_range(key);	\
+		}	\
+	};
+
+#define MULTI_INDEX_MAP_DECLTYPE_(struct_, member_)	\
+	__typeof__(((struct_ *)1)->member_)
+
+#define UNIQUE_INDEX(member_)	\
+	::boost::multi_index::ordered_unique<	\
+		::boost::multi_index::member<value_type,	\
+			MULTI_INDEX_MAP_DECLTYPE_(value_type, member_),	\
+			&value_type::member_	\
+		>	\
+	>
+
+#define MULTI_INDEX(member_)	\
+	::boost::multi_index::ordered_non_unique<	\
+		::boost::multi_index::member<value_type,	\
+			MULTI_INDEX_MAP_DECLTYPE_(value_type, member_),	\
+			&value_type::member_	\
+		>	\
+	>
+
+#endif
