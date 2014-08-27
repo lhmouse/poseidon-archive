6542123091c3d19f7f219fa563f59f8082d179ad
lhmouse <lh_mouse@126.com>
2014-08-27 09:55:03 +0800

    Removed test stuffs.

--- src/main/player_session.cpp
+++ src/main/player_session.cpp
@@ -4,26 +4,6 @@
 #include "vint50.hpp"
 using namespace Poseidon;
 
-
-#include "profiler.hpp"
-
-__attribute__((__noinline__)) void baz(){
-	PROFILE_ME;
-	::usleep(10000);
-}
-__attribute__((__noinline__)) void bar(){
-	PROFILE_ME;
-	::usleep(20000);
-	baz();
-	::usleep(10000);
-}
-__attribute__((__noinline__)) void foo(){
-	PROFILE_ME;
-	::usleep(10000);
-	bar();
-	::usleep(20000);
-}
-
 PlayerSession::PlayerSession(ScopedFile &socket)
 	: TcpPeer(socket), m_payloadLen(-1)
 {
@@ -41,12 +21,6 @@ void PlayerSession::onReadAvail(const void *data, std::size_t size){
 	for(int i = 0; i < 10; ++i){
 		char data = '0' + i;
 		send(&data, 1);
-	}
-	foo();
-	AUTO(ss, Profiler::snapshot());
-	for(AUTO(it, ss.begin()); it != ss.end(); ++it){
-		LOG_WARNING("file = ", it->file, ", line = ", it->line, ", samples = ", it->samples,
-			", usTotal = ", it->usTotal, ", usExclusive = ", it->usExclusive);
 	}
 }
 void PlayerSession::perform() const {
