8007258f3bf97383be4a9bffe6b9d17b27cbcada
lhmouse <lh_mouse@126.com>
2014-09-02 14:55:21 +0800

    TEMP

---
+++ src/main/protocol_base.hpp
@@ -0,0 +1,34 @@
+#ifndef POSEIDON_PROTOCOL_BASE_HPP_
+#define POSEIDON_PROTOCOL_BASE_HPP_
+
+#include <string>
+#include <vector>
+#include "stream_buffer.hpp"
+
+namespace Poseidon {
+
+}
+
+#define PROTOCOL(name_, id_, ...)	\
+	struct name_ {	\
+		enum {	\
+			PROTOCOL_ID = id_	\
+		};	\
+		__VA_ARGS__	\
+	};
+
+// Protocol Fields
+#define PF_VINT50(name_)	\
+	long long name_;	\
+
+#define PF_VUINT50(name_)	\
+	unsigned long long name_;	\
+
+#define PF_STRING(name_)	\
+	::std::string name_;	\
+
+#define PF_REPEAT(...)	\
+	struct REP
+	::std::vector<
+
+#endif
