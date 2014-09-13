75d27650442b0ac3549f6dc16edf52912b5c308b
lhmouse <lh_mouse@126.com>
2014-09-13 19:19:34 +0800

    Added -D_GLIBCXX_DEBUG_PEDANTIC.

--- reconfig_debug_cxx11.sh
+++ reconfig_debug_cxx11.sh
@@ -2,5 +2,6 @@
 
 mkdir -p m4
 autoreconf -if
-CPPFLAGS='-D_GLIBCXX_DEBUG' CFLAGS='-O0 -g -std=c99' CXXFLAGS='-O0 -g -std=c++11'	\
+CPPFLAGS='-D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC'	\
+	CFLAGS='-O0 -g -std=c99' CXXFLAGS='-O0 -g -std=c++11'	\
 	./configure --enable-shared --disable-static
