b9384e6e6e13b31467de856257942f49aa1dd907
lhmouse <lh_mouse@126.com>
2014-09-15 11:11:50 +0800

    Atomic updates.

--- src/main/atomic.hpp
+++ src/main/atomic.hpp
@@ -4,7 +4,7 @@
 namespace Poseidon {
 
 template<typename T>
-struct Identity_ {
+struct AtomicIdentity_ {
 	typedef T type;
 };
 
@@ -15,32 +15,32 @@ T atomicLoad(const volatile T &mem){
 	return mem;
 }
 template<typename T>
-void atomicStore(volatile T &mem, typename Identity_<T>::type val){
+void atomicStore(volatile T &mem, typename AtomicIdentity_<T>::type val){
 	mem = val;
 	volatile int barrier;
 	__sync_lock_release(&barrier);
 }
 
 template<typename T>
-T atomicAdd(volatile T &mem, typename Identity_<T>::type val){
+T atomicAdd(volatile T &mem, typename AtomicIdentity_<T>::type val){
 	return __sync_add_and_fetch(&mem, val);
 }
 template<typename T>
-T atomicSub(volatile T &mem, typename Identity_<T>::type val){
+T atomicSub(volatile T &mem, typename AtomicIdentity_<T>::type val){
 	return __sync_sub_and_fetch(&mem, val);
 }
 
 template<typename T>
-T atomicCmpExchange(volatile T &mem, typename Identity_<T>::type cmp,
-	typename Identity_<T>::type xchg) throw()
+T atomicCmpExchange(volatile T &mem, typename AtomicIdentity_<T>::type cmp,
+	typename AtomicIdentity_<T>::type xchg) throw()
 {
 	return __sync_val_compare_and_swap(&mem, cmp, xchg);
 }
 template<typename T>
-T atomicExchange(volatile T &mem, typename Identity_<T>::type xchg){
+T atomicExchange(volatile T &mem, typename AtomicIdentity_<T>::type xchg){
 	T cmp = mem;
 	for(;;){
-		const T old = __sync_val_compare_and_swap(&mem, cmp, xchg);
+		const T old = atomicCmpExchange(mem, cmp, xchg);
 		if(old == cmp){
 			break;
 		}
