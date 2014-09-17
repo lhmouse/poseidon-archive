eaceba50e018bb6773030d989126945ec29977d6
lhmouse <lh_mouse@126.com>
2014-09-17 16:39:39 +0800

    Revert "Added database tuple."
    This reverts commit 93d5925177579b332e18bc2fae6ddbecd4f08a52.

--- src/main/player/protocol_generator.hpp
+++ src/main/player/protocol_generator.hpp
@@ -30,18 +30,22 @@ struct ProtocolBase {
 namespace PROTOCOL_NAMESPACE {
 #endif
 
-struct PROTOCOL_NAME
-	: public ::Poseidon::ProtocolBase
-{
+struct PROTOCOL_NAME : public ProtocolBase {
 
 #undef FIELD_VINT50
 #undef FIELD_VUINT50
 #undef FIELD_STRING
 #undef FIELD_ARRAY
 
-#define FIELD_VINT50(name_)		long long name_;
-#define FIELD_VUINT50(name_)	unsigned long long name_;
-#define FIELD_STRING(name_)		::std::string name_;
+#define FIELD_VINT50(name_)	\
+	long long name_;
+
+#define FIELD_VUINT50(name_)	\
+	unsigned long long name_;
+
+#define FIELD_STRING(name_)	\
+	::std::string name_;
+
 #define FIELD_ARRAY(name_, fields_)	\
 	struct ElementOf ## name_ ## _ {	\
 		fields_	\
@@ -50,20 +54,26 @@ struct PROTOCOL_NAME
 
 	PROTOCOL_FIELDS
 
-	PROTOCOL_NAME()
-		: ::Poseidon::ProtocolBase()
+	PROTOCOL_NAME() throw()
+		: ProtocolBase()
 
 #undef FIELD_VINT50
 #undef FIELD_VUINT50
 #undef FIELD_STRING
 #undef FIELD_ARRAY
 
-#define FIELD_VINT50(name_)				, name_()
-#define FIELD_VUINT50(name_)			, name_()
-#define FIELD_STRING(name_)				, name_()
-#define FIELD_ARRAY(name_, fields_)		, name_()
+#define FIELD_VINT50(name_)	\
+	, name_()
 
-		PROTOCOL_FIELDS
+#define FIELD_VUINT50(name_)	\
+	, name_()
+
+#define FIELD_STRING(name_)	\
+	, name_()
+
+#define FIELD_ARRAY(name_, fields_)	\
+	, name_()
+
 	{
 	}
 
@@ -71,11 +81,9 @@ struct PROTOCOL_NAME
 		typedef PROTOCOL_NAME Cur_;
 		const Cur_ &cur_ = *this;
 		::Poseidon::StreamBufferWriteIterator write_(buffer_);
+
 		(void)cur_;
 		(void)write_;
-
-		unsigned long long count_;
-		(void)count_;
 
 #undef FIELD_VINT50
 #undef FIELD_VUINT50
@@ -93,9 +101,9 @@ struct PROTOCOL_NAME
 		write_ = ::std::copy(cur_.name_.begin(), cur_.name_.end(), write_);
 
 #define FIELD_ARRAY(name_, fields_)	\
-		count_ = cur_.name_.size();	\
-		::Poseidon::vuint50ToBinary(count_, write_);	\
-		for(unsigned long long i = 0; i < count_; ++i){	\
+		const unsigned long long countOf ## name_ ## _ = cur_.name_.size();	\
+		::Poseidon::vuint50ToBinary(countOf ## name_ ## _, write_);	\
+		for(unsigned long long i = 0; i < countOf ## name_ ## _; ++i){	\
 			typedef Cur_::ElementOf ## name_ ## _ Element_;	\
 			const Element_ &element_ = cur_.name_[i];	\
 			typedef Element_ Cur_;	\
@@ -111,11 +119,9 @@ struct PROTOCOL_NAME
 		typedef PROTOCOL_NAME Cur_;
 		Cur_ &cur_ = *this;
 		::Poseidon::StreamBufferReadIterator read_(buffer_);
+
 		(void)cur_;
 		(void)read_;
-
-		unsigned long long count_;
-		(void)count_;
 
 #undef FIELD_VINT50
 #undef FIELD_VUINT50
@@ -133,23 +139,25 @@ struct PROTOCOL_NAME
 		}
 
 #define FIELD_STRING(name_)	\
-		if(!::Poseidon::vuint50FromBinary(count_, read_, buffer_.size())){	\
+		unsigned long long countOf ## name_ ## _;	\
+		if(!::Poseidon::vuint50FromBinary(countOf ## name_ ## _, read_, buffer_.size())){	\
 			THROW_EOS_;	\
 		}	\
-		if(buffer_.size() < count_){	\
+		if(buffer_.size() < countOf ## name_ ## _){	\
 			THROW_EOS_;	\
 		}	\
-		cur_.name_.resize(count_);	\
-		for(unsigned long long i = 0; i < count_; ++i){	\
+		cur_.name_.resize(countOf ## name_ ## _);	\
+		for(unsigned long long i = 0; i < countOf ## name_ ## _; ++i){	\
 			cur_.name_[i] = buffer_.get();	\
 		}
 
 #define FIELD_ARRAY(name_, fields_)	\
-		if(!::Poseidon::vuint50FromBinary(count_, read_, buffer_.size())){	\
+		unsigned long long countOf ## name_ ## _;	\
+		if(!::Poseidon::vuint50FromBinary(countOf ## name_ ## _, read_, buffer_.size())){	\
 			THROW_EOS_;  \
 		}	\
 		cur_.name_.clear();	\
-		for(unsigned long long i = 0; i < count_; ++i){	\
+		for(unsigned long long i = 0; i < countOf ## name_ ## _; ++i){	\
 			typedef Cur_::ElementOf ## name_ ## _ Element_;	\
 			cur_.name_.push_back(Element_());	\
 			Element_ &element_ = cur_.name_.back();	\
