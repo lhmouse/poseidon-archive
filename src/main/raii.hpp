3c7f0f372d8e431620da669241cae9f7072ad755
lhmouse <lh_mouse@126.com>
2014-09-19 11:48:26 +0800

    Removed unused shared handle.

--- src/main/raii.hpp
+++ src/main/raii.hpp
@@ -87,139 +87,7 @@ public:
 };
 
 template<typename CloserT>
-class SharedHandle {
-public:
-	typedef DECLTYPE(DECLREF(CloserT)()) Handle;
-
-private:
-	typedef ScopedHandle<CloserT> Scoped;
-
-	struct Control {
-		Scoped h;
-		volatile unsigned long ref;
-	};
-
-private:
-	Control *m_control;
-
-public:
-	SharedHandle() NOEXCEPT
-		: m_control(0)
-	{
-	}
-	explicit SharedHandle(Handle handle)
-		: m_control(0)
-	{
-		reset(handle);
-	}
-	SharedHandle(const SharedHandle &rhs) NOEXCEPT
-		: m_control(0)
-	{
-		reset(rhs);
-	}
-	SharedHandle &operator=(Handle handle){
-		Scoped tmp(handle);
-		reset(tmp);
-		return *this;
-	}
-	SharedHandle &operator=(const SharedHandle &rhs) NOEXCEPT {
-		reset(rhs);
-		return *this;
-	}
-#ifdef POSEIDON_CXX11
-	SharedHandle(SharedHandle &&rhs) noexcept
-		: m_control(0)
-	{
-		reset(std::move(rhs));
-	}
-	SharedHandle &operator=(SharedHandle &&rhs) noexcept {
-		reset(std::move(rhs));
-		return *this;
-	}
-#endif
-	~SharedHandle() NOEXCEPT {
-		reset();
-	}
-
-public:
-	Handle get() const NOEXCEPT {
-		if(!m_control){
-			return CloserT()();
-		}
-		return m_control->h.get();
-	}
-	bool unique() const NOEXCEPT {
-		if(!m_control){
-			return false;
-		}
-		volatile int barrier;
-		__sync_lock_test_and_set(&barrier, 1);
-		return m_control->ref == 1;
-	}
-	void reset() NOEXCEPT {
-		if(!m_control){
-			return;
-		}
-		if(__sync_sub_and_fetch(&m_control->ref, 1) == 0){
-			delete m_control;
-		}
-		m_control = 0;
-	}
-	void reset(Handle handle){
-		Scoped tmp(handle);
-		reset(tmp);
-	}
-	void reset(typename Scoped::Move scoped){
-		if(!scoped){
-			reset();
-			return;
-		}
-		if(unique()){
-			m_control->h.reset(scoped);
-		} else {
-			reset();
-			m_control = new Control;
-			m_control->h.reset(scoped);
-			m_control->ref = 1;
-			volatile int barrier;
-			__sync_lock_release(&barrier);
-		}
-	}
-	void reset(const SharedHandle &rhs) NOEXCEPT {
-		if(&rhs == this){
-			return;
-		}
-		if(rhs.m_control){
-			m_control = rhs.m_control;
-			__sync_add_and_fetch(&m_control->ref, 1);
-		} else {
-			reset();
-		}
-	}
-#ifdef POSEIDON_CXX11
-	void reset(SharedHandle &&rhs) noexcept {
-		swap(rhs);
-	}
-#endif
-	void swap(SharedHandle &rhs) NOEXCEPT {
-		using std::swap;
-		swap(m_control, rhs.m_control);
-	}
-#ifdef POSEIDON_CXX11
-	explicit
-#endif
-	operator bool() const NOEXCEPT {
-		return get() != CloserT()();
-	}
-};
-
-template<typename CloserT>
 void swap(ScopedHandle<CloserT> &lhs, ScopedHandle<CloserT> &rhs) NOEXCEPT {
-	lhs.swap(rhs);
-}
-
-template<typename CloserT>
-void swap(SharedHandle<CloserT> &lhs, SharedHandle<CloserT> &rhs) NOEXCEPT {
 	lhs.swap(rhs);
 }
 
@@ -250,13 +118,6 @@ DEFINE_RATIONAL_OPERATOR_(ScopedHandle, >)
 DEFINE_RATIONAL_OPERATOR_(ScopedHandle, <=)
 DEFINE_RATIONAL_OPERATOR_(ScopedHandle, >=)
 
-DEFINE_RATIONAL_OPERATOR_(SharedHandle, ==)
-DEFINE_RATIONAL_OPERATOR_(SharedHandle, !=)
-DEFINE_RATIONAL_OPERATOR_(SharedHandle, <)
-DEFINE_RATIONAL_OPERATOR_(SharedHandle, >)
-DEFINE_RATIONAL_OPERATOR_(SharedHandle, <=)
-DEFINE_RATIONAL_OPERATOR_(SharedHandle, >=)
-
 #undef DEFINE_RATIONAL_OPERATOR_
 
 extern void closeFile(int fd) NOEXCEPT;
@@ -271,7 +132,6 @@ struct FileCloser {
 };
 
 typedef ScopedHandle<FileCloser> ScopedFile;
-typedef SharedHandle<FileCloser> SharedFile;
 
 }
 
