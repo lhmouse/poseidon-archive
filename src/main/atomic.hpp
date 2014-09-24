a0fba9eb0492271600a2354ee879ef48cb62d51f
lhmouse <lh_mouse@126.com>
2014-09-24 18:04:08 +0800

    Atomic updates. Settle for gcc 4.7 __atomic built-ins.

--- src/main/atomic.hpp
+++ src/main/atomic.hpp
@@ -1,6 +1,10 @@
 #ifndef POSEIDON_ATOMIC_HPP_
 #define POSEIDON_ATOMIC_HPP_
 
+#if __GNUC__ * 100 + __GNUC_MINOR__ > 407
+#	define GCC_HAS_ATOMIC_
+#endif
+
 namespace Poseidon {
 
 template<typename T>
@@ -10,47 +14,76 @@ struct AtomicIdentity_ {
 
 template<typename T>
 inline T atomicLoad(const volatile T &mem) throw() {
+#ifdef GCC_HAS_ATOMIC_
+	return __atomic_load_n(&mem, __ATOMIC_SEQ_CST);
+#else
 	volatile int barrier;
 	__sync_lock_test_and_set(&barrier, 1);
 	return mem;
+#endif
 }
 template<typename T>
 inline void atomicStore(volatile T &mem, typename AtomicIdentity_<T>::type val) throw() {
+#ifdef GCC_HAS_ATOMIC_
+	__atomic_store_n(&mem, val, __ATOMIC_SEQ_CST);
+#else
 	mem = val;
 	volatile int barrier;
 	__sync_lock_release(&barrier);
+#endif
 }
 
 inline void atomicSynchronize() throw() {
+#ifdef GCC_HAS_ATOMIC_
+	__atomic_thread_fence(__ATOMIC_SEQ_CST);
+#else
 	__sync_synchronize();
+#endif
 }
 
 template<typename T>
 inline T atomicAdd(volatile T &mem, typename AtomicIdentity_<T>::type val) throw() {
+#ifdef GCC_HAS_ATOMIC_
+	return __atomic_add_fetch(&mem, val, __ATOMIC_SEQ_CST);
+#else
 	return __sync_add_and_fetch(&mem, val);
+#endif
 }
 template<typename T>
 inline T atomicSub(volatile T &mem, typename AtomicIdentity_<T>::type val) throw() {
+#ifdef GCC_HAS_ATOMIC_
+	return __atomic_sub_fetch(&mem, val, __ATOMIC_SEQ_CST);
+#else
 	return __sync_sub_and_fetch(&mem, val);
+#endif
 }
 
 template<typename T>
 inline T atomicCmpExchange(volatile T &mem, typename AtomicIdentity_<T>::type cmp,
 	typename AtomicIdentity_<T>::type xchg) throw()
 {
+#ifdef GCC_HAS_ATOMIC_
+	__atomic_compare_exchange_n(&mem, &cmp, xchg, false, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST);
+	return cmp;
+#else
 	return __sync_val_compare_and_swap(&mem, cmp, xchg);
+#endif
 }
 template<typename T>
 inline T atomicExchange(volatile T &mem, typename AtomicIdentity_<T>::type xchg) throw() {
+#ifdef GCC_HAS_ATOMIC_
+	return __atomic_exchange_n(&mem, xchg, __ATOMIC_SEQ_CST);
+#else
 	T cmp = mem;
 	for(;;){
-		const T old = atomicCmpExchange(mem, cmp, xchg);
+		const T old = __sync_val_compare_and_swap(&mem, cmp, xchg);
 		if(old == cmp){
 			break;
 		}
 		cmp = old;
 	}
 	return cmp;
+#endif
 }
 
 }
