bc03093a54764eba28811c3866eb64bee246857b
lhmouse <lh_mouse@126.com>
2014-10-08 20:08:18 +0800

    Use conf.rc from ./config/ directory.

--- run_server.sh
+++ run_server.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ "$1" == "-d" ]; then
-	sudo libtool --mode=execute gdb ./sbin/poseidon
+	sudo libtool --mode=execute gdb ./sbin/poseidon `pwd`/config/conf.rc
 elif [ "$1" == "-v" ]; then
-	sudo libtool --mode=execute valgrind ./sbin/poseidon
+	sudo libtool --mode=execute valgrind ./sbin/poseidon `pwd`/config/conf.rc
 else
-	sudo ./sbin/poseidon
+	sudo ./sbin/poseidon `pwd`/config/conf.rc
 fi
