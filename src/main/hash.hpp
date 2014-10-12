6e0f8d98dccaa86907fc2a0ae96e84dd62a6faad
lhmouse <lh_mouse@126.com>
2014-10-13 02:05:38 +0800

    ##TEMP hash.

---
+++ src/main/hash.hpp
@@ -0,0 +1,19 @@
+// 这个文件是 MCF 的一部分。
+// 有关具体授权说明，请参阅 MCFLicense.txt。
+// Copyleft 2013 - 2014, LH_Mouse. All wrongs reserved.
+
+#ifndef POSEIDON_HTTP_HASH_HPP_
+#define POSEIDON_HTTP_HASH_HPP_
+
+#include <cstddef>
+#include <cstdint>
+
+namespace Poseidon {
+
+extern std::uint32_t crc32Sum(const void *data, std::size_t size);
+extern void md5Sum(unsigned char (&hash)[16], const void *data, std::size_t size);
+extern void sha1Sum(unsigned char (&hash)[20], const void *data, std::size_t size);
+
+}
+
+#endif
