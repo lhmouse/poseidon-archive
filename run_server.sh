47f4ac99196ea823fa3b32d32844c97a7bb879d7
lhmouse <lh_mouse@126.com>
2014-10-18 00:20:30 +0800

    run_server script update.

--- run_server.sh
+++ run_server.sh
@@ -3,7 +3,7 @@
 if [ "$1" == "-d" ]; then
 	sudo libtool --mode=execute gdb --args ./sbin/poseidon `pwd`/config/conf.rc
 elif [ "$1" == "-v" ]; then
-	sudo libtool --mode=execute valgrind ./sbin/poseidon `pwd`/config/conf.rc
+	sudo libtool --mode=execute valgrind ./sbin/poseidon `pwd`/config/conf.rc --log-file=valgrind.log
 else
 	sudo ./sbin/poseidon `pwd`/config/conf.rc
 fi
