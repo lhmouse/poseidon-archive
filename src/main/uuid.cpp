a36763a3f59db548e04f5893472f5ba8f0d3508f
lhmouse <lh_mouse@126.com>
2014-10-23 18:24:00 +0800

    Bug fixes.

--- src/main/uuid.cpp
+++ src/main/uuid.cpp
@@ -1,12 +1,24 @@
 #include "../precompiled.hpp"
 #include "uuid.hpp"
+#include <boost/thread/mutex.hpp>
+#include <boost/random/mersenne_twister.hpp>
 #include <boost/uuid/uuid_generators.hpp>
 #include <boost/uuid/uuid_io.hpp>
 #include <boost/lexical_cast.hpp>
+#include "utilities.hpp"
 using namespace Poseidon;
 
+namespace {
+
+boost::mutex g_mutex;
+boost::random::mt19937 g_rng(getMonoClock());
+boost::uuids::random_generator g_generator(g_rng);
+
+}
+
 Uuid Uuid::createRandom(){
-	return Uuid(boost::uuids::random_generator()());
+	const boost::mutex::scoped_lock lock(g_mutex);
+	return Uuid(g_generator());
 }
 Uuid Uuid::createFromString(const std::string &str){
 	return Uuid(boost::lexical_cast<boost::uuids::uuid>(str));
