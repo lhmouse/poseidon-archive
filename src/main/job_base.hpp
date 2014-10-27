9f3aa9ffa5759ce4cd43285d4f2cbb781da490ff
lhmouse <lh_mouse@126.com>
2014-10-27 15:22:13 +0800

    Made JobBase noncopyable.

--- src/main/job_base.hpp
+++ src/main/job_base.hpp
@@ -1,12 +1,13 @@
 #ifndef POSEIDON_JOB_BASE_HPP_
 #define POSEIDON_JOB_BASE_HPP_
 
+#include <boost/noncopyable.hpp>
 #include <boost/enable_shared_from_this.hpp>
 
 namespace Poseidon {
 
-class JobBase
-	: public boost::enable_shared_from_this<JobBase>
+class JobBase : boost::noncopyable
+	, public boost::enable_shared_from_this<JobBase>
 {
 public:
 	virtual ~JobBase();
