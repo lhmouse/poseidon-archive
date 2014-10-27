0eb28d75f149c024966314581ecf30645338a6d1
lhmouse <lh_mouse@126.com>
2014-10-27 13:46:27 +0800

    Added --leak-check=full when running valgrind.

--- run_server.sh
+++ run_server.sh
@@ -5,7 +5,7 @@ var=`pwd`'/var'
 if [ "$1" == "-d" ]; then
 	sudo libtool --mode=execute gdb --args ./sbin/poseidon $var
 elif [ "$1" == "-v" ]; then
-	sudo libtool --mode=execute valgrind --log-file='valgrind.log' ./sbin/poseidon $var
+	sudo libtool --mode=execute valgrind --leak-check=full --log-file='valgrind.log' ./sbin/poseidon $var
 else
 	sudo ./sbin/poseidon $var
 fi
