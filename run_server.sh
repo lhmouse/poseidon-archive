295c1efe9921c46cf84dca56d0f9eaf9cbd546b6
lhmouse <lh_mouse@126.com>
2014-11-04 20:07:47 +0800

    Removed sudo from run_server.

--- run_server.sh
+++ run_server.sh
@@ -3,9 +3,9 @@
 var=`pwd`'/var'
 
 if [ "$1" == "-d" ]; then
-	sudo libtool --mode=execute gdb --args ./sbin/poseidon $var
+	libtool --mode=execute gdb --args ./sbin/poseidon $var
 elif [ "$1" == "-v" ]; then
-	sudo libtool --mode=execute valgrind --leak-check=full --log-file='valgrind.log' ./sbin/poseidon $var
+	libtool --mode=execute valgrind --leak-check=full --log-file='valgrind.log' ./sbin/poseidon $var
 else
-	sudo ./sbin/poseidon $var
+	./sbin/poseidon $var
 fi
