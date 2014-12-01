46b3896337f402459765e354c99144e068ceebaf
lhmouse <lh_mouse@126.com>
2014-12-01 17:11:56 +0800

    Minor.

--- src/main/singletons/mysql_daemon.cpp
+++ src/main/singletons/mysql_daemon.cpp
@@ -315,18 +315,21 @@ private:
 				DEBUG_THROW(Exception, "No threads available");
 			}
 			// 指派给最空闲的线程。
-			std::size_t idle = 0;
+			std::size_t picked = 0;
+			AUTO(minBusyTime, g_threads.front()->getBusyTime());
+			LOG_POSEIDON_TRACE("Busy time of MySQL thread 0: ", minBusyTime);
 			for(std::size_t i = 1; i < g_threads.size(); ++i){
+				const AUTO(myBusyTime, g_threads[i]->getBusyTime());
+				LOG_POSEIDON_TRACE("Busy time of MySQL thread ", i, ": ", myBusyTime);
 				// 即使是无符号 64 位整数发生了进位，这个结果也是对的。
 				// 参考 TCP 中对于报文流水号的处理。
-				if(static_cast<boost::int64_t>(
-					g_threads[idle]->getBusyTime() - g_threads[i]->getBusyTime()) > 0)
-				{
-					idle = i;
+				if(static_cast<boost::int64_t>(minBusyTime - myBusyTime) > 0){
+					picked = i;
+					minBusyTime = myBusyTime;
 				}
 			}
-			m_thread = g_threads[idle];
-			LOG_POSEIDON_DEBUG("Assigned table `", m_table, "` to thread ", idle);
+			m_thread = g_threads[picked];
+			LOG_POSEIDON_DEBUG("Assigned table `", m_table, "` to thread ", picked);
 		}
 		++m_count;
 		return AssignmentLock(this);
