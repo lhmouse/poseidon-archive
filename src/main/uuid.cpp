67f2db3ae46d7a242c8762b193aeaefe70bff5e3
lhmouse <lh_mouse@126.com>
2014-12-25 13:45:12 +0800

    Bug fix.

--- src/main/uuid.cpp
+++ src/main/uuid.cpp
@@ -5,80 +5,11 @@
 #include "uuid.hpp"
 #include "utilities.hpp"
 #include "atomic.hpp"
+#include "endian.hpp"
+#include "exception.hpp"
 using namespace Poseidon;
 
 namespace {
-
-struct BrokenDownUuid {
-	boost::uint32_t first;
-	boost::uint16_t second;
-	boost::uint16_t third;
-	boost::uint64_t bytes;
-
-	void store(Uuid &dst) const {
-		AUTO(write, dst.getBytes());
-
-#define DO_STORE(part_)	\
-		{	\
-			VALUE_TYPE(part_) temp = part_;	\
-			unsigned i = sizeof(part_);	\
-			do {	\
-				--i;	\
-				*write = temp >> (i * CHAR_BIT);	\
-				++write;	\
-			} while(i != 0);	\
-		}
-
-		DO_STORE(first)
-		DO_STORE(second)
-		DO_STORE(third)
-		DO_STORE(bytes)
-	}
-	void load(const Uuid &src){
-		AUTO(read, src.getBytes());
-
-#define DO_LOAD(part_)	\
-		{	\
-			VALUE_TYPE(part_) temp = 0;	\
-			unsigned i = sizeof(part_);	\
-			do {	\
-				--i;	\
-				temp |= (unsigned long long)*read << (i * CHAR_BIT);	\
-				++read;	\
-			} while(i != 0);	\
-			part_ = temp;	\
-		}
-
-		DO_LOAD(first)
-		DO_LOAD(second)
-		DO_LOAD(third)
-		DO_LOAD(bytes)
-	}
-
-	unsigned print(char *text) const {
-		const boost::uint16_t fourth = bytes >> 48;
-		const boost::uint64_t fifth = (bytes << 16) >> 16;
-		return std::sprintf(text, "%08lX-%04X-%04X-%04X-%012llX",
-			(unsigned long)first, (unsigned)second, (unsigned)third,
-			(unsigned)fourth, (unsigned long long)fifth);
-	}
-	bool scan(const char *text){
-		unsigned long u1;
-		unsigned short u2, u3, u4;
-		unsigned long long u5;
-		char probe;
-		if(std::sscanf(text, "%lX-%hX-%hX-%hX-%llX%c",
-			&u1, &u2, &u3, &u4, &u5, &probe) != 5)
-		{
-			return false;
-		}
-		first = u1;
-		second = u2;
-		third = u3;
-		bytes = ((unsigned long long)u4 << 48) | u5;
-		return true;
-	}
-};
 
 volatile boost::uint32_t g_autoInc = 0;
 
@@ -86,42 +17,107 @@ volatile boost::uint32_t g_autoInc = 0;
 
 Uuid Uuid::generate(){
 	const AUTO(now, getUtcTime());
-	BrokenDownUuid temp;
-	temp.first = now >> 28;
-	temp.second = now >> 12;
-	temp.third = now & 0x0FFF; // version = 0
-	temp.bytes = 0xC0000000 | atomicAdd(g_autoInc, 1, ATOMIC_RELAXED); // veriant = 3
-	temp.bytes <<= 32;
-	temp.bytes |= rand32();
-	Uuid ret;
-	temp.store(ret);
+	const AUTO(unique, atomicAdd(g_autoInc, 1, ATOMIC_RELAXED));
+
+	Uuid ret
+#ifdef POSEIDON_CXX11
+		(nullptr)	// 不初始化。
+#endif
+		;
+	storeBe(ret.m_storage.u32[0], now >> 28);
+	storeBe(ret.m_storage.u16[2], now >> 12);
+	storeBe(ret.m_storage.u16[3], now & 0x0FFFu); // 版本 = 0
+	storeBe(ret.m_storage.u32[2], 0xC0000000u | unique); // 变种 = 3
+	storeBe(ret.m_storage.u32[3], rand32());
 	return ret;
 }
 
-std::string Uuid::toString() const {
-	BrokenDownUuid temp;
-	temp.load(*this);
-	char text[64];
-	const unsigned len = temp.print(text);
-	return std::string(text, len);
-}
-bool Uuid::fromString(const std::string &str){
-	BrokenDownUuid temp;
-	if(!temp.scan(str.c_str())){
-		return false;
+Uuid::Uuid(const char (&str)[37]){
+	if(!fromString(str)){
+		DEBUG_THROW(Exception, "Invalid UUID string");
 	}
-	temp.store(*this);
+}
+
+void Uuid::toString(char (&str)[37], bool upperCase) const {
+	AUTO(read, begin());
+	char *write = str;
+
+#define PRINT(count_)	\
+	for(std::size_t i_ = 0; i_ < count_; ++i_){	\
+		const unsigned byte = *(read++);	\
+		unsigned ch = byte >> 4;	\
+		if(ch <= 9){	\
+			ch += '0';	\
+		} else if(upperCase){	\
+			ch += 'A' - 0x0A;	\
+		} else {	\
+			ch += 'a' - 0x0A;	\
+		}	\
+		*(write++) = ch;	\
+		ch = byte & 0x0F;	\
+		if(ch <= 9){	\
+			ch += '0';	\
+		} else if(upperCase){	\
+			ch += 'A' - 0x0A;	\
+		} else {	\
+			ch += 'a' - 0x0A;	\
+		}	\
+		*(write++) = ch;	\
+	}
+
+	PRINT(4)	*(write++) = '-';
+	PRINT(2)	*(write++) = '-';
+	PRINT(2)	*(write++) = '-';
+	PRINT(2)	*(write++) = '-';
+	PRINT(6)	*(write++) = 0;
+}
+bool Uuid::fromString(const char (&str)[37]){
+	const char *read = str;
+	AUTO(write, begin());
+
+#define SCAN(count_)	\
+	for(std::size_t i_ = 0; i_ < count_; ++i_){	\
+		unsigned byte;	\
+		unsigned ch = (unsigned char)*(read++);	\
+		if(('0' <= ch) && (ch <= '9')){	\
+			ch -= '0';	\
+		} else if(('A' <= ch) && (ch <= 'F')){	\
+			ch -= 'A' - 0x0A;	\
+		} else if(('a' <= ch) && (ch <= 'f')){	\
+			ch -= 'a' - 0x0A;	\
+		} else {	\
+			return false;	\
+		}	\
+		byte = ch << 4;	\
+		ch = (unsigned char)*(read++);	\
+		if(('0' <= ch) && (ch <= '9')){	\
+			ch -= '0';	\
+		} else if(('A' <= ch) && (ch <= 'F')){	\
+			ch -= 'A' - 0x0A;	\
+		} else if(('a' <= ch) && (ch <= 'f')){	\
+			ch -= 'a' - 0x0A;	\
+		} else {	\
+			return false;	\
+		}	\
+		byte |= ch;	\
+		*(write++) = byte;	\
+	}
+
+	SCAN(4)	if(*(read++) != '-'){ return false; }
+	SCAN(2)	if(*(read++) != '-'){ return false; }
+	SCAN(2)	if(*(read++) != '-'){ return false; }
+	SCAN(2)	if(*(read++) != '-'){ return false; }
+	SCAN(6)	if(*(read++) != 0){ return false; }
+
 	return true;
 }
 
 namespace Poseidon {
 
 std::ostream &operator<<(std::ostream &os, const Uuid &rhs){
-	BrokenDownUuid temp;
-	temp.load(rhs);
-	char text[64];
-	const unsigned len = temp.print(text);
-	return os.write(text, len);
+	char temp[37];
+	rhs.toString(temp);
+	return os.write(temp, 36);
 }
 
 }
--- src/main/uuid.hpp
+++ src/main/uuid.hpp
@@ -4,9 +4,10 @@
 #ifndef POSEIDON_UUID_HPP_
 #define POSEIDON_UUID_HPP_
 
-#include <string>
+#include "cxx_ver.hpp"
 #include <iosfwd>
 #include <cstring>
+#include <boost/cstdint.hpp>
 
 namespace Poseidon {
 
@@ -15,56 +16,73 @@ public:
 	static Uuid generate();
 
 private:
-	unsigned char m_bytes[16];
+	union {
+		unsigned char bytes[16];
+		boost::uint16_t u16[8];
+		boost::uint32_t u32[4];
+		boost::uint64_t u64[2];
+	} m_storage;
 
 public:
-	Uuid(){
-		std::memset(m_bytes, 0, 16);
+	CONSTEXPR Uuid()
+		: m_storage()
+	{
 	}
+#ifdef POSEIDON_CXX11
+	explicit Uuid(std::nullptr_t){
+	}
+#endif
 	explicit Uuid(const unsigned char (&bytes)[16]){
-		std::memcpy(m_bytes, bytes, 16);
+		std::memcpy(m_storage.bytes, bytes, 16);
 	}
-	explicit Uuid(const std::string &str){
-		fromString(str);
-	}
+	explicit Uuid(const char (&str)[37]);
 
 public:
-	std::string toString() const;
-	bool fromString(const std::string &str);
+	const unsigned char *begin() const {
+		return m_storage.bytes;
+	}
+	unsigned char *begin(){
+		return m_storage.bytes;
+	}
+	const unsigned char *end() const {
+		return m_storage.bytes + 16;
+	}
+	unsigned char *end(){
+		return m_storage.bytes + 16;
+	}
+	CONSTEXPR std::size_t size() const {
+		return 16;
+	}
 
-	const unsigned char (&getBytes() const)[16] {
-		return m_bytes;
-	}
-	unsigned char (&getBytes())[16] {
-		return m_bytes;
-	}
+	void toString(char (&str)[37], bool upperCase = true) const;
+	bool fromString(const char (&str)[37]);
 
 public:
 	const unsigned char &operator[](unsigned index) const {
-		return m_bytes[index];
+		return m_storage.bytes[index];
 	}
 	unsigned char &operator[](unsigned index){
-		return m_bytes[index];
+		return m_storage.bytes[index];
 	}
 };
 
 inline bool operator==(const Uuid &lhs, const Uuid &rhs){
-	return std::memcmp(lhs.getBytes(), rhs.getBytes(), 16) == 0;
+	return std::memcmp(lhs.begin(), rhs.begin(), 16) == 0;
 }
 inline bool operator!=(const Uuid &lhs, const Uuid &rhs){
-	return std::memcmp(lhs.getBytes(), rhs.getBytes(), 16) != 0;
+	return std::memcmp(lhs.begin(), rhs.begin(), 16) != 0;
 }
 inline bool operator<(const Uuid &lhs, const Uuid &rhs){
-	return std::memcmp(lhs.getBytes(), rhs.getBytes(), 16) < 0;
+	return std::memcmp(lhs.begin(), rhs.begin(), 16) < 0;
 }
 inline bool operator>(const Uuid &lhs, const Uuid &rhs){
-	return std::memcmp(lhs.getBytes(), rhs.getBytes(), 16) > 0;
+	return std::memcmp(lhs.begin(), rhs.begin(), 16) > 0;
 }
 inline bool operator<=(const Uuid &lhs, const Uuid &rhs){
-	return std::memcmp(lhs.getBytes(), rhs.getBytes(), 16) <= 0;
+	return std::memcmp(lhs.begin(), rhs.begin(), 16) <= 0;
 }
 inline bool operator>=(const Uuid &lhs, const Uuid &rhs){
-	return std::memcmp(lhs.getBytes(), rhs.getBytes(), 16) >= 0;
+	return std::memcmp(lhs.begin(), rhs.begin(), 16) >= 0;
 }
 
 extern std::ostream &operator<<(std::ostream &os, const Uuid &rhs);
