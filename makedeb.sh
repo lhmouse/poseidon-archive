87d405f48aefe07a958dcf503f6a8e7a78ebaf8d
lhmouse <lh_mouse@126.com>
2014-12-27 12:35:42 +0800

    Minor.

--- makedeb.sh
+++ makedeb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo checkinstall --backup=no --nodoc -y --pkgname='libposeidon-main' --maintainer='lh_mouse@126.com'
+sudo checkinstall --backup=no --nodoc -y --pkgname='poseidon' --maintainer='lh_mouse@126.com'
