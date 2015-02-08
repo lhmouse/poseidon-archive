bfdbdde83d4c49a981eee4b4a9b1dc783fe62a17
lhmouse <lh_mouse@126.com>
2015-02-08 16:10:08 +0800

    Changed from sbin to bin.

--- run_server.sh
+++ run_server.sh
@@ -3,9 +3,9 @@
 etc=`pwd`'/etc'
 
 if [ "$1" == "-d" ]; then
-	libtool --mode=execute gdb --args ./sbin/poseidon $etc/poseidon
+	libtool --mode=execute gdb --args ./bin/poseidon $etc/poseidon
 elif [ "$1" == "-v" ]; then
-	libtool --mode=execute valgrind --leak-check=full --log-file='valgrind.log' ./sbin/poseidon $etc/poseidon
+	libtool --mode=execute valgrind --leak-check=full --log-file='valgrind.log' ./bin/poseidon $etc/poseidon
 else
-	./sbin/poseidon $etc/poseidon
+	./bin/poseidon $etc/poseidon
 fi
