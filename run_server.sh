e716704d4ccce219cec109dbedae4e8e2916becb
lhmouse <lh_mouse@126.com>
2015-04-29 01:27:36 +0800

    Minor.

--- run_server.sh
+++ run_server.sh
@@ -3,9 +3,9 @@
 etc=`pwd`'/etc'
 
 if [ "$1" == "-d" ]; then
-	libtool --mode=execute gdb --args ./bin/poseidon $etc/poseidon
+	./libtool --mode=execute gdb --args ./bin/poseidon $etc/poseidon
 elif [ "$1" == "-v" ]; then
-	libtool --mode=execute valgrind --leak-check=full --log-file='valgrind.log' ./bin/poseidon $etc/poseidon
+	./libtool --mode=execute valgrind --leak-check=full --log-file='valgrind.log' ./bin/poseidon $etc/poseidon
 else
 	./bin/poseidon $etc/poseidon
 fi
