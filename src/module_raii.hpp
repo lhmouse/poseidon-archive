fa7598e6eaba8b1649d135c7e52097a5afb67901
lhmouse <lh_mouse@126.com>
2015-05-09 10:05:42 +0800

    Minor.

--- src/module_raii.hpp
+++ src/module_raii.hpp
@@ -59,7 +59,7 @@ public:
 			} const stub_;	\
 		}	\
 	}	\
-	void TOKEN_CAT3(ModuleRaii_, __LINE__, _Impl_)::Stub_::init(::Poseidon::HandleStack & (handles_) ) const
+	void TOKEN_CAT3(ModuleRaii_, __LINE__, _Impl_)::Stub_::init(::Poseidon::HandleStack & handles_) const
 
 #define MODULE_RAII(handles_)	MODULE_RAII_PRIORITY(handles_, 65535)
 
