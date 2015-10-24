44ea20c92c3738e4babb12b6a2ae6130ebbf72e9
lhmouse <lh_mouse@126.com>
2015-10-25 00:12:07 +0800

    Bug fix.

--- src/singletons/timer_daemon.cpp
+++ src/singletons/timer_daemon.cpp
@@ -32,10 +32,6 @@ struct TimerItem : NONCOPYABLE {
 };
 
 namespace {
-	const boost::uint64_t MILLISECS_PER_HOUR	= 3600 * 1000;
-	const boost::uint64_t MILLISECS_PER_DAY		= 24 * MILLISECS_PER_HOUR;
-	const boost::uint64_t MILLISECS_PER_WEEK	= 7 * MILLISECS_PER_DAY;
-
 	class TimerJob : public JobBase {
 	private:
 		const boost::weak_ptr<TimerItem> m_item;
@@ -204,21 +200,24 @@ boost::shared_ptr<TimerItem> TimerDaemon::registerTimer(
 boost::shared_ptr<TimerItem> TimerDaemon::registerHourlyTimer(
 	unsigned minute, unsigned second, TimerCallback callback, bool isAsync)
 {
-	const AUTO(delta, getLocalTime() - (minute * 60ull + second));
-	return registerTimer(MILLISECS_PER_HOUR - delta % MILLISECS_PER_HOUR, MILLISECS_PER_HOUR, STD_MOVE(callback), isAsync);
+	const boost::uint64_t MS_PER_HOUR = 3600 * 1000;
+	const AUTO(delta, getLocalTime() - (minute * 60ull + second) * 1000);
+	return registerTimer(MS_PER_HOUR - delta % MS_PER_HOUR, MS_PER_HOUR, STD_MOVE(callback), isAsync);
 }
 boost::shared_ptr<TimerItem> TimerDaemon::registerDailyTimer(
 	unsigned hour, unsigned minute, unsigned second, TimerCallback callback, bool isAsync)
 {
-	const AUTO(delta, getLocalTime() - (hour * 3600ull + minute * 60ull + second));
-	return registerTimer(MILLISECS_PER_DAY - delta % MILLISECS_PER_DAY, MILLISECS_PER_DAY, STD_MOVE(callback), isAsync);
+	const boost::uint64_t MS_PER_DAY = 24 * 3600 * 1000;
+	const AUTO(delta, getLocalTime() - (hour * 3600ull + minute * 60ull + second) * 1000);
+	return registerTimer(MS_PER_DAY - delta % MS_PER_DAY, MS_PER_DAY, STD_MOVE(callback), isAsync);
 }
 boost::shared_ptr<TimerItem> TimerDaemon::registerWeeklyTimer(
 	unsigned dayOfWeek, unsigned hour, unsigned minute, unsigned second, TimerCallback callback, bool isAsync)
 {
+	const boost::uint64_t MS_PER_WEEK = 7 * 24 * 3600 * 1000;
 	// 注意 1970-01-01 是星期四。
-	const AUTO(delta, getLocalTime() - ((dayOfWeek + 3) * 86400ull + hour * 3600ull + minute * 60ull + second));
-	return registerTimer(MILLISECS_PER_WEEK - delta % MILLISECS_PER_WEEK, MILLISECS_PER_WEEK, STD_MOVE(callback), isAsync);
+	const AUTO(delta, getLocalTime() - ((dayOfWeek + 3) * 86400ull + hour * 3600ull + minute * 60ull + second) * 1000);
+	return registerTimer(MS_PER_WEEK - delta % MS_PER_WEEK, MS_PER_WEEK, STD_MOVE(callback), isAsync);
 }
 
 void TimerDaemon::setAbsoluteTime(
