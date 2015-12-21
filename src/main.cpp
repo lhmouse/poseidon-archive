41263aa89845fd459d5b273eadd7ee5b603731c0
lhmouse <lh_mouse@126.com>
2015-12-21 15:04:15 +0800

    Minor.

--- src/main.cpp
+++ src/main.cpp
@@ -89,7 +89,7 @@ namespace {
 			JobDispatcher::do_modal();
 		} catch(...){
 			JobDispatcher::pump_all();
-			Logger::set_mask(0, -1ull);
+			Logger::set_mask(0, Logger::LV_INFO | Logger::LV_WARNING | Logger::LV_ERROR | Logger::LV_FATAL);
 			throw;
 		}
 		JobDispatcher::pump_all();
