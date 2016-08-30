d2571c610b15e6f99228b11f533c3658f1e86106
lhmouse <lh_mouse@126.com>
2016-08-30 14:25:46 +0800

    More.

--- third/build_mongo-c-driver_deb.sh
+++ third/build_mongo-c-driver_deb.sh
@@ -2,13 +2,15 @@
 
 set -e
 
-pkgname="libmongoc"
+prefix="/usr/local"
+
+pkgname="libmongoc-dev"
 pkgversion="1.4.0"
 pkglicense="Apache"
 pkggroup="http://mongoc.org/"
 pkgsource="https://github.com/mongodb/mongo-c-driver/releases/download/${pkgversion}/mongo-c-driver-${pkgversion}.tar.gz"
 maintainer="lh_mouse"
-provides="libmongoc-1.0,libbson-1.0"
+provides="libmongoc-1.0-dev,libbson-1.0-dev"
 
 dstdir="$(pwd)"
 tempdir="$(mktemp -d)"
@@ -28,8 +30,18 @@ wget -O "${_archive}" "${pkgsource}"
 tar -xzvf "${_archive}"
 
 cd "$(basename "${_archive}" ".tar.gz")"
-CFLAGS='-O3' ./configure --disable-automatic-init-and-cleanup --prefix=/usr/local
+CFLAGS="-O3" ./configure --disable-automatic-init-and-cleanup --prefix="${prefix}"
 make -j4
+
+mkdir -p "${prefix}/bin"
+mkdir -p "${prefix}/docs"
+mkdir -p "${prefix}/etc"
+mkdir -p "${prefix}/include"
+mkdir -p "${prefix}/lib"
+mkdir -p "${prefix}/man"
+mkdir -p "${prefix}/sbin"
+mkdir -p "${prefix}/share"
+
 checkinstall -y --pkgname="${pkgname}" --pkgversion="${pkgversion}" --pkglicense="${pkglicense}" --pkggroup="${pkggroup}"	\
 	--pkgsource="${pkgsource}" --maintainer="${maintainer}" --provides="${provides}"
-mv *.deb "${dstdir}"/
+mv *.deb "${dstdir}/"
--- third/build_mysql-connector-c_deb.sh
+++ third/build_mysql-connector-c_deb.sh
@@ -2,13 +2,15 @@
 
 set -e
 
-pkgname="libmysqlclient"
+prefix="/usr/local"
+
+pkgname="libmysqlclient-dev"
 pkgversion="6.1.6"
 pkglicense="GPL or Commercial (https://www.mysql.com/about/legal/licensing/oem/)"
 pkggroup="https://dev.mysql.com/"
 pkgsource="https://dev.mysql.com/get/Downloads/Connector-C/mysql-connector-c-${pkgversion}-src.tar.gz"
 maintainer="lh_mouse"
-provides="libmysqlclient"
+provides="libmysqlclient-dev"
 
 dstdir="$(pwd)"
 tempdir="$(mktemp -d)"
@@ -30,6 +32,16 @@ tar -xzvf "${_archive}"
 cd "$(basename "${_archive}" ".tar.gz")"
 CFLAGS="-O3" cmake . -DDEFAULT_CHARSET=utf8 -DDEFAULT_COLLATION=utf8_general_ci -DCMAKE_INSTALL_PREFIX=/usr/local
 make -j4
+
+mkdir -p "${prefix}/bin"
+mkdir -p "${prefix}/docs"
+mkdir -p "${prefix}/etc"
+mkdir -p "${prefix}/include"
+mkdir -p "${prefix}/lib"
+mkdir -p "${prefix}/man"
+mkdir -p "${prefix}/sbin"
+mkdir -p "${prefix}/share"
+
 checkinstall -y --pkgname="${pkgname}" --pkgversion="${pkgversion}" --pkglicense="${pkglicense}" --pkggroup="${pkggroup}"	\
 	--pkgsource="${pkgsource}" --maintainer="${maintainer}" --provides="${provides}"
-mv *.deb "${dstdir}"/
+mv *.deb "${dstdir}/"
