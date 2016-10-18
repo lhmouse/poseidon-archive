a3e23ae9df2d97bcb5f76b5e57fea77e2a879f79
lhmouse <lh_mouse@126.com>
2016-10-18 12:27:22 +0800

    [MINOR] Fixed WebSocket HTTP response description.

--- src/websocket/handshake.cpp
+++ src/websocket/handshake.cpp
@@ -9,44 +9,46 @@
 
 namespace Poseidon {
 
-namespace WebSocket {
-	Http::ResponseHeaders make_handshake_response(const Http::RequestHeaders &request_headers){
-		Http::ResponseHeaders ret;
-
+namespace {
+	Http::StatusCode real_make_handshake_response(OptionalMap &headers, const Http::RequestHeaders &request_headers){
 		if(request_headers.version < 10001){
 			LOG_POSEIDON(Logger::SP_MAJOR | Logger::LV_DEBUG, "HTTP 1.1 is required to use WebSocket");
-			ret.version = 10000;
-			ret.status_code = Http::ST_VERSION_NOT_SUPPORTED;
-			return ret;
+			return Http::ST_VERSION_NOT_SUPPORTED;
 		}
-		ret.version = 10001;
-
 		if(request_headers.verb != Http::V_GET){
 			LOG_POSEIDON(Logger::SP_MAJOR | Logger::LV_DEBUG, "Must use GET verb to use WebSocket");
-			ret.status_code = Http::ST_METHOD_NOT_ALLOWED;
-			return ret;
+			return Http::ST_METHOD_NOT_ALLOWED;
 		}
-
 		AUTO_REF(websocket_version, request_headers.headers.get("Sec-WebSocket-Version"));
 		if(websocket_version != "13"){
 			LOG_POSEIDON(Logger::SP_MAJOR | Logger::LV_DEBUG, "Unknown HTTP header Sec-WebSocket-Version: ", websocket_version);
-			ret.status_code = Http::ST_BAD_REQUEST;
-			return ret;
+			return Http::ST_BAD_REQUEST;
 		}
-
 		AUTO(sec_web_socket_key, request_headers.headers.get("Sec-WebSocket-Key"));
 		if(sec_web_socket_key.empty()){
 			LOG_POSEIDON(Logger::SP_MAJOR | Logger::LV_DEBUG, "No Sec-WebSocket-Key specified.");
-			ret.status_code = Http::ST_BAD_REQUEST;
-			return ret;
+			return Http::ST_BAD_REQUEST;
 		}
 		sec_web_socket_key += "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
 		const AUTO(sha1, sha1_hash(sec_web_socket_key));
 		AUTO(sec_web_socket_accept, Http::base64_encode(sha1.data(), sha1.size()));
-		ret.headers.set(sslit("Upgrade"), "websocket");
-		ret.headers.set(sslit("Connection"), "Upgrade");
-		ret.headers.set(sslit("Sec-WebSocket-Accept"), STD_MOVE(sec_web_socket_accept));
-		ret.status_code = Http::ST_SWITCHING_PROTOCOLS;
+		headers.set(sslit("Upgrade"), "websocket");
+		headers.set(sslit("Connection"), "Upgrade");
+		headers.set(sslit("Sec-WebSocket-Accept"), STD_MOVE(sec_web_socket_accept));
+		return Http::ST_SWITCHING_PROTOCOLS;
+	}
+}
+
+namespace WebSocket {
+	Http::ResponseHeaders make_handshake_response(const Http::RequestHeaders &request_headers){
+		OptionalMap headers;
+		const AUTO(status_code, real_make_handshake_response(headers, request_headers));
+
+		Http::ResponseHeaders ret;
+		ret.version = 10001;
+		ret.status_code = status_code;
+		ret.reason = Http::get_status_code_desc(ret.status_code).desc_short;
+		ret.headers = STD_MOVE(headers);
 		return ret;
 	}
 }
