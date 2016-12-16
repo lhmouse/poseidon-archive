0372325e3e17dc845bde0d19dcf19c6e4f94e471
lhmouse <lh_mouse@126.com>
2016-12-16 12:10:28 +0800

    .

--- src/zlib.cpp
+++ src/zlib.cpp
@@ -185,7 +185,7 @@ StreamBuffer Inflator::finalize(){
 	m_context->stream.next_in = m_context->temp;
 	m_context->stream.avail_in = 0;
 	for(;;){
-		const int err_code = ::inflate(&(m_context->stream), Z_FULL_FLUSH);
+		const int err_code = ::inflate(&(m_context->stream), Z_NO_FLUSH);
 		if(err_code == Z_STREAM_END){
 			break;
 		}
