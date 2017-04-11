aadb2abe5ad03feecce77b53b2d7488e443934a4
lhmouse <lh_mouse@126.com>
2017-04-11 10:16:17 +0800

    New version numbers.

--- makedeb.sh
+++ makedeb.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-sudo checkinstall --backup=no --nodoc -y --strip=no --stripso=no	\
-	--pkgname='poseidon' --maintainer='lh_mouse@126.com' --pkgversion='0.1'
+_pkgname="poseidon"
+_maintainer=$(git config --get user.email)
+_pkgversion=$(printf "0.%u.%s" "$(git rev-list --count HEAD)" "$(git rev-parse --short HEAD)")
+
+sudo checkinstall --backup=no --nodoc -y --strip=no --stripso=no --pkgname="$_pkgname" --maintainer="$_maintainer" --pkgversion="$_pkgversion"
