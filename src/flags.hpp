04b6d6b1845c9babe98c32dc3390c5e150357872
lhmouse <lh_mouse@126.com>
2017-06-24 16:12:41 +0800

    Add static assertions in flags.hpp.

--- src/flags.hpp
+++ src/flags.hpp
@@ -5,30 +5,43 @@
 #define POSEIDON_FLAGS_HPP_
 
 #include "cxx_util.hpp"
+#include <boost/type_traits/is_enum.hpp>
+#include <boost/type_traits/is_integral.hpp>
+#include <boost/static_assert.hpp>
 
 namespace Poseidon {
 
 template<typename T>
 T &add_flags(T &val, typename Identity<T>::type flags){
+	BOOST_STATIC_ASSERT((boost::is_enum<T>::value || boost::is_integral<T>::value));
+
 	val |= flags;
 	return val;
 }
 template<typename T>
 T &remove_flags(T &val, typename Identity<T>::type flags){
+	BOOST_STATIC_ASSERT((boost::is_enum<T>::value || boost::is_integral<T>::value));
+
 	val &= ~flags;
 	return val;
 }
 
 template<typename T>
 bool has_all_flags_of(const T &val, typename Identity<T>::type flags){
+	BOOST_STATIC_ASSERT((boost::is_enum<T>::value || boost::is_integral<T>::value));
+
 	return (val & flags) == flags;
 }
 template<typename T>
 bool has_any_flags_of(const T &val, typename Identity<T>::type flags){
+	BOOST_STATIC_ASSERT((boost::is_enum<T>::value || boost::is_integral<T>::value));
+
 	return (val & flags) != 0;
 }
 template<typename T>
 bool has_none_flags_of(const T &val, typename Identity<T>::type flags){
+	BOOST_STATIC_ASSERT((boost::is_enum<T>::value || boost::is_integral<T>::value));
+
 	return (val & flags) == 0;
 }
 
