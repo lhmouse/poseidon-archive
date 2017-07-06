3f6545f2f4ceacbac7bc46a1c8edeb52a170a98f
lhmouse <lh_mouse@126.com>
2017-07-06 15:16:58 +0800

    Tweak MainConfig.

--- src/singletons/main_config.cpp
+++ src/singletons/main_config.cpp
@@ -6,45 +6,40 @@
 #include "../config_file.hpp"
 #include "../log.hpp"
 #include "../system_exception.hpp"
+#include <limits.h>
+#include <stdlib.h>
 
 namespace Poseidon {
 
 namespace {
-	const std::string g_main_conf("main.conf");
-
 	ConfigFile g_config;
-
-	std::string get_real_path(const char *path){
-		std::string ret;
-		char *real_path = NULLPTR;
-		try {
-			real_path = ::realpath(path, NULLPTR);
-			if(!real_path){
-				LOG_POSEIDON_ERROR("Could not resolve path: ", path);
-				DEBUG_THROW(SystemException);
-			}
-			ret = real_path;
-			::free(real_path);
-		} catch(...){
-			::free(real_path);
-			throw;
-		}
-		return ret;
-	}
 }
 
 void MainConfig::set_run_path(const char *path){
-	const AUTO(real_path, get_real_path(path));
-	LOG_POSEIDON(Logger::SP_MAJOR | Logger::LV_INFO, "Setting working directory: real_path = ", real_path);
-	if(::chdir(real_path.c_str()) != 0){
+	char *const real_path = ::realpath(path, NULLPTR);
+	if(!real_path){
+		LOG_POSEIDON_ERROR("Could not resolve path: ", path);
 		DEBUG_THROW(SystemException);
+	}
+	try {
+		LOG_POSEIDON(Logger::SP_MAJOR | Logger::LV_INFO, "Setting working directory: ", real_path);
+		if(::chdir(real_path) != 0){
+			LOG_POSEIDON_ERROR("Could not set working directory: ", real_path);
+			DEBUG_THROW(SystemException);
+		}
+		::free(real_path);
+	} catch(...){
+		::free(real_path);
+		throw;
 	}
 }
 
 void MainConfig::reload(){
-
-	LOG_POSEIDON(Logger::SP_MAJOR | Logger::LV_INFO, "Loading main config file: ", g_main_conf);
-	g_config = ConfigFile(g_main_conf);
+	static CONSTEXPR const char MAIN_CONF[] = "main.conf";
+	LOG_POSEIDON(Logger::SP_MAJOR | Logger::LV_INFO, "Loading main config file: ", MAIN_CONF);
+	ConfigFile config(MAIN_CONF);
+	LOG_POSEIDON(Logger::SP_MAJOR | Logger::LV_INFO, "Done loading main config file: ", MAIN_CONF);
+	g_config.swap(config);
 }
 const ConfigFile &MainConfig::get_config(){
 	return g_config;
