6acb8ba4080826da0fd4b503d2b71d7812f5327d
lhmouse <lh_mouse@126.com>
2017-09-02 00:28:59 +0800

    Minor adjustment of makedeb.sh.

--- makedeb.sh
+++ makedeb.sh
@@ -4,4 +4,5 @@ _pkgname="poseidon"
 _maintainer=$(git config --get user.email)
 _pkgversion=$(printf "0.%u.%s" "$(git rev-list --count HEAD)" "$(git rev-parse --short HEAD)")
 
-sudo checkinstall --backup=no --nodoc -y --strip=no --stripso=no --pkgname="$_pkgname" --maintainer="$_maintainer" --pkgversion="$_pkgversion"
+sudo checkinstall --backup=no --nodoc -y --strip=no --stripso=no	\
+	--pkgname="${_pkgname}" --maintainer="${_maintainer}" --pkgversion="${_pkgversion}"
