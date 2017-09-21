2880b87d6fff1e4dc1031db68f2f8089cda5ef11
lhmouse <lh_mouse@126.com>
2017-09-21 18:03:07 +0800

    Exclude files in ~ for third libraries.

--- third/build_mongo-c-driver_deb.sh
+++ third/build_mongo-c-driver_deb.sh
@@ -36,7 +36,7 @@ sudo mkdir -p "${prefix}/man"
 sudo mkdir -p "${prefix}/sbin"
 sudo mkdir -p "${prefix}/share/doc"
 
-sudo checkinstall --backup=no --nodoc -y	\
+sudo checkinstall --backup=no --nodoc -y --exclude=~	\
 	--pkgname="${pkgname}" --pkgversion="${pkgversion}" --pkglicense="${pkglicense}" --pkggroup="${pkggroup}"	\
 	--pkgsource="${pkgsource}" --maintainer="${maintainer}" --provides="${provides}"
 sudo mv *.deb "${dstdir}/"
