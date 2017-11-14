7bd0e61ab0550be16e91532e5412828b10d2e316
Liu Hao <lh_mouse@126.com>
2017-11-14 14:11:23 +0800

    Update random.cpp.

--- src/random.cpp
+++ src/random.cpp
@@ -3,36 +3,33 @@
 
 #include "precompiled.hpp"
 #include "random.hpp"
+#include "atomic.hpp"
 
 namespace Poseidon {
 
 namespace {
-	__thread boost::uint64_t t_random_seed = 0;
+	volatile boost::uint64_t s_seed;
 }
 
 boost::uint32_t random_uint32(){
-	boost::uint64_t seed = t_random_seed, tsc;
-	__asm__ __volatile__(
-		"rdtsc \n"
-#ifdef __x86_64__
-		"shlq $32, %%rdx \n"
-		"orq %%rdx, %%rax \n"
-		: "=a"(tsc) : : "dx"
-#else
-		: "=A"(tsc) : :
-#endif
-	);
-	seed ^= tsc;
-	// MMIX by Donald Knuth
-	seed = seed * 6364136223846793005ull + 1442695040888963407ull;
-	t_random_seed = seed;
-	return seed >> 32;
+	boost::uint64_t old_seed, new_seed;
+	{
+		old_seed = atomic_load(s_seed, ATOMIC_RELAXED);
+		do {
+			new_seed = old_seed;
+			new_seed ^= __builtin_ia32_rdtsc();
+			// MMIX by Donald Knuth
+			new_seed *= 6364136223846793005u;
+			new_seed += 1442695040888963407u;
+		} while(!atomic_compare_exchange(s_seed, old_seed, new_seed, ATOMIC_RELAXED, ATOMIC_RELAXED));
+	}
+	return new_seed >> 32;
 }
 boost::uint64_t random_uint64(){
-	return (static_cast<boost::uint64_t>(random_uint32()) << 32) | random_uint32();
+	return ((boost::uint64_t)random_uint32() << 32) + random_uint32();
 }
 double random_double(){
-	return static_cast<boost::int64_t>(random_uint64() >> 11) / 0x1p53;
+	return (boost::int64_t)(random_uint64() >> 1) / 0x1p63;
 }
 
 }
