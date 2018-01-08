792228f92bca7c248330c6a7a8a6cc656b74bd5e
Liu Hao <lh_mouse@126.com>
2018-01-08 11:50:46 +0800

    Fix typo in comments.

--- src/singletons/main_config.cpp
+++ src/singletons/main_config.cpp
@@ -32,7 +32,7 @@ namespace {
 void MainConfig::set_run_path(const char *path){
 	UniqueHandle<RealPathDeleter> real_path;
 	DEBUG_THROW_UNLESS(real_path.reset(::realpath(path, NULLPTR)), SystemException);
-	LOG_POSEIDON(Logger::SP_MAJOR | Logger::LV_INFO, "Set new working directory: ", real_path);
+	LOG_POSEIDON(Logger::SP_MAJOR | Logger::LV_INFO, "Setting new working directory: ", real_path);
 	DEBUG_THROW_UNLESS(::chdir(real_path.get()) == 0, SystemException);
 }
 void MainConfig::reload(){
