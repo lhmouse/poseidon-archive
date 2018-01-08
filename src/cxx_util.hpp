e8c72950dda9e3bea5bb35ee769f73a9b528db81
Liu Hao <lh_mouse@126.com>
2018-01-08 15:01:15 +0800

    Minor.

--- src/cxx_util.hpp
+++ src/cxx_util.hpp
@@ -10,13 +10,13 @@
 #define STRIP_FIRST_(x_, ...)       __VA_ARGS__
 #define STRIP_FIRST(...)            STRIP_FIRST_(__VA_ARGS__)
 
-#define TOKEN_CAT2_(x_, y_)         x_ ## y_
+#define TOKEN_CAT2_(x_, y_)         x_##y_
 #define TOKEN_CAT2(x_, y_)          TOKEN_CAT2_(x_, y_)
 
-#define TOKEN_CAT3_(x_, y_, z_)     x_ ## y_ ## z_
+#define TOKEN_CAT3_(x_, y_, z_)     x_##y_##z_
 #define TOKEN_CAT3(x_, y_, z_)      TOKEN_CAT3_(x_, y_, z_)
 
-#define MAGIC_LN_2_(ln_)            Poseidon_magic_ ## ln_ ## X_
+#define MAGIC_LN_2_(ln_)            Poseidon_magic_##ln_##X_
 #define MAGIC_LN_1_(ln_)            MAGIC_LN_2_(ln_)
 
 #define UNIQUE_ID                   MAGIC_LN_1_(__COUNTER__)
