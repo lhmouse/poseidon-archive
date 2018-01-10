0906273cbe90f913def72ea9b042f7035ac53b02
Liu Hao <lh_mouse@126.com>
2018-01-10 17:40:12 +0800

    Use `--addso=yes` when making DEB packages.

--- makedeb.sh
+++ makedeb.sh
@@ -4,5 +4,5 @@ _pkgname="poseidon"
 _maintainer=$(git config --get user.email)
 _pkgversion=$(printf "0.%u.%s" "$(git rev-list --count HEAD)" "$(git rev-parse --short HEAD)")
 
-sudo checkinstall --backup=no --nodoc -y --strip=no --stripso=no	\
+sudo checkinstall --backup=no --nodoc -y --strip=no --stripso=no --addso=yes	\
 	--pkgname="${_pkgname}" --maintainer="${_maintainer}" --pkgversion="${_pkgversion}"
