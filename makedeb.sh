290bfc7dfe88bf7b8e7cb101ccc87da5e2c28c75
Liu Hao <lh_mouse@126.com>
2018-01-12 14:25:09 +0800

    Update makedeb.sh.

--- makedeb.sh
+++ makedeb.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 _pkgname="poseidon"
-_maintainer=$(git config --get user.email)
-_pkgversion=$(git describe 2>/dev/null || printf "0.%u.%s" "$(git rev-list --count HEAD)" "$(git rev-parse --short HEAD)")
+_maintainer="$(git config --get user.email)"
+_pkgversion="$(git describe 2>/dev/null || printf "0.%u.%s" "$(git rev-list --count HEAD)" "$(git rev-parse --short HEAD)")"
 
 sudo checkinstall --backup=no --nodoc -y --strip=no --stripso=no --addso=yes	\
 	--pkgname="${_pkgname}" --maintainer="${_maintainer}" --pkgversion="${_pkgversion}"
