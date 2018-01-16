06e89c93ebdd7ec62b8d555e6a0311a39757bbd9
Liu Hao <lh_mouse@126.com>
2018-01-16 09:59:11 +0800

    Add UdpClientBase.

--- src/udp_client_base.hpp
+++ src/udp_client_base.hpp
@@ -1,8 +1,8 @@
 // 这个文件是 Poseidon 服务器应用程序框架的一部分。
 // Copyleft 2014 - 2018, LH_Mouse. All wrongs reserved.
 
-#ifndef POSEIDON_UDP_SERVER_BASE_HPP_
-#define POSEIDON_UDP_SERVER_BASE_HPP_
+#ifndef POSEIDON_UDP_CLIENT_BASE_HPP_
+#define POSEIDON_UDP_CLIENT_BASE_HPP_
 
 #include <boost/shared_ptr.hpp>
 #include <boost/container/deque.hpp>
@@ -13,14 +13,14 @@
 
 namespace Poseidon {
 
-class UdpServerBase : public SocketBase {
+class UdpClientBase : public SocketBase {
 private:
 	mutable Mutex m_send_mutex;
 	mutable boost::container::deque<std::pair<SockAddr, StreamBuffer> > m_send_queue;
 
 public:
-	explicit UdpServerBase(const SockAddr &addr);
-	~UdpServerBase();
+	explicit UdpClientBase(const SockAddr &addr);
+	~UdpClientBase();
 
 protected:
 	// 注意，只能在 epoll 线程中调用这些函数。
@@ -31,6 +31,12 @@ protected:
 	virtual void on_message_too_large(const SockAddr &sock_addr, StreamBuffer data);
 
 public:
+	bool has_been_shutdown_read() const NOEXCEPT OVERRIDE;
+	bool has_been_shutdown_write() const NOEXCEPT OVERRIDE;
+	bool shutdown_read() NOEXCEPT OVERRIDE;
+	bool shutdown_write() NOEXCEPT OVERRIDE;
+	void force_shutdown() NOEXCEPT OVERRIDE;
+
 	bool send(const SockAddr &sock_addr, StreamBuffer buffer);
 };
 
