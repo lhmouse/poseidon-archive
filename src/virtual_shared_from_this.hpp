fbfde4d5773aae1cc4dbcdf8e170a28e690a4dbb
Liu Hao <lh_mouse@126.com>
2018-01-16 09:42:53 +0800

    Minor.

--- src/virtual_shared_from_this.hpp
+++ src/virtual_shared_from_this.hpp
@@ -4,31 +4,31 @@
 #ifndef POSEIDON_VIRTUAL_SHARED_FROM_THIS_HPP_
 #define POSEIDON_VIRTUAL_SHARED_FROM_THIS_HPP_
 
-#include <cassert>
 #include <boost/enable_shared_from_this.hpp>
 #include <boost/shared_ptr.hpp>
 #include <boost/type_traits/is_virtual_base_of.hpp>
 #include <boost/static_assert.hpp>
+#include <typeinfo>
 
 namespace Poseidon {
 
 class VirtualSharedFromThis : public boost::enable_shared_from_this<VirtualSharedFromThis> {
 public:
 	// 定义在别处。参考源文件中的注释。
-	virtual ~VirtualSharedFromThis() = 0;
+	virtual ~VirtualSharedFromThis();
 
 public:
 	template<typename DerivedT>
 	boost::shared_ptr<const DerivedT> virtual_shared_from_this() const {
 		BOOST_STATIC_ASSERT((boost::is_virtual_base_of<VirtualSharedFromThis, DerivedT>::value));
-
-		return boost::shared_ptr<const DerivedT>(shared_from_this(), &dynamic_cast<const DerivedT &>(*this));
+		const AUTO(ptr, dynamic_cast<const DerivedT *>(this));
+		return ptr ? boost::shared_ptr<const DerivedT>(shared_from_this(), ptr) : throw std::bad_cast();
 	}
 	template<typename DerivedT>
 	boost::shared_ptr<DerivedT> virtual_shared_from_this(){
 		BOOST_STATIC_ASSERT((boost::is_virtual_base_of<VirtualSharedFromThis, DerivedT>::value));
-
-		return boost::shared_ptr<DerivedT>(shared_from_this(), &dynamic_cast<DerivedT &>(*this));
+		const AUTO(ptr, dynamic_cast<DerivedT *>(this));
+		return ptr ? boost::shared_ptr<DerivedT>(shared_from_this(), ptr) : throw std::bad_cast();
 	}
 
 	template<typename DerivedT>
