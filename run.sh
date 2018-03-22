b25cc2925ae94b6fddf2a832b1f68792b3390169
Liu Hao <lh_mouse@126.com>
2018-03-22 17:40:23 +0800

    Update 'run.sh'.

--- run.sh
+++ run.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-_etc="$(pwd)/etc/poseidon"
+set -e
 
-if [ "$1" == "-d" ]; then
-	./libtool --mode=execute gdb --args ./bin/poseidon "${_etc}"
-elif [ "$1" == "-v" ]; then
-	./libtool --mode=execute valgrind --leak-check=full --log-file='valgrind.log' ./bin/poseidon "${_etc}"
-elif [ "$1" == "-vgdb" ]; then
-	./libtool --mode=execute valgrind --vgdb=yes --vgdb-error=0 --leak-check=full --log-file='valgrind.log' ./bin/poseidon "${_etc}"
+_type="$1"
+test -z "${_type}" || shift
+
+_runpath="$(find $(pwd) -path '*/lib/.libs' -type d -print0 | sed -r 's/\x00/:/g')"
+_confpath="$(pwd)/etc/poseidon"
+
+export LD_LIBRARY_PATH="${_runpath}"
+
+if [[ "{_type}" == "-d" ]]; then
+	./libtool --mode=execute gdb --args poseidon "${_confpath}" $*
+elif [[ "{_type}" == "-v" ]]; then
+	./libtool --mode=execute valgrind --leak-check=full --log-file='valgrind.log' poseidon "${_confpath}" $*
+elif [[ "{_type}" == "-vgdb" ]]; then
+	./libtool --mode=execute valgrind --vgdb=yes --vgdb-error=0 --leak-check=full --log-file='valgrind.log' poseidon "${_confpath}" $*
 else
-	./bin/poseidon "${_etc}"
+	poseidon "${_confpath}" $*
 fi
