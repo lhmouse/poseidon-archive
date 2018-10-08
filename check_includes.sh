e2ac368502928fed1130f48e5105f24f730af63c
Liu Hao <lh_mouse@126.com>
2018-10-08 22:15:42 +0800

    Update 'check_includes.sh'.

--- check_includes.sh
+++ check_includes.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -e
 
-function check_one(){
-	_file="$1"
-	_cmd="gcc -std=c++11 -x c++ -c -o /dev/null"
-	echo "Checking \`#include\` directives:  ${_cmd}  \"${_file}\""
-	${_cmd}  "${_file}"
-}
+function check_one()
+  {
+    _file="$1"
+    _cmd="gcc -std=c++11 -x c++ -c -o /dev/null"
+    echo "Checking \`#include\` directives:  ${_cmd}  \"${_file}\""
+    ${_cmd}  "${_file}"
+  }
 
 export -f check_one
-find -L "poseidon/src/" -name "*.hpp" -print0 | xargs -0 -I {} bash -c 'check_one "$@"' "$0" {}
+find -L "poseidon/src/" -name "*.hpp" -print0 | xargs -0 -I {} bash -ec 'check_one "$@"' "$0" {}
