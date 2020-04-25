d0dd1afd588b76842087461cf4128ef4285caa9e
Liu Hao <lh_mouse@126.com>
2020-04-25 21:36:28 +0800

    *: Empty project

---
+++ poseidon/src/precompiled.hpp
@@ -0,0 +1,25 @@
+// This file is part of Poseidon.
+// Copyleft 2020, LH_Mouse. All wrongs reserved.
+
+#ifndef POSEIDON_PRECOMPILED_HPP_
+#define POSEIDON_PRECOMPILED_HPP_
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <asteria/precompiled.hpp>
+
+#include <string>
+#include <vector>
+#include <deque>
+#include <list>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <memory>
+#include <functional>
+
+#endif
