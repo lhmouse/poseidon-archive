843923532144d7aa17548a763cbfe931dd855e99
Liu Hao <lh_mouse@126.com>
2020-04-25 22:04:05 +0800

    asteria: Update

---
+++ ci/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -xe
+
+# setup
+export CXX=${CXX:-"g++"}
+export CXXFLAGS='-O2 -g0 -std=gnu++14 -fno-gnu-keywords -Wno-zero-as-null-pointer-constant'
+
+# build
+${CXX} --version
+mkdir -p m4
+autoreconf -ifv
+./configure --disable-silent-rules --enable-debug-checks --disable-static
+make -j$(nproc)
+
+# test
+./check_includes.sh
+make -j$(nproc) check || (cat ./test-suite.log; false)
