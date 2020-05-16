3ab78c3a64a11f6100c74996727fdb5019eb8b46
Liu Hao <lh_mouse@126.com>
2020-05-16 23:57:12 +0800

    utilities: Use `__PRETTY_FUNCTION__`

--- poseidon/src/utilities.hpp
+++ poseidon/src/utilities.hpp
@@ -50,7 +50,8 @@ noexcept
 // Note the format string must be a string literal.
 #define POSEIDON_XLOG_(level, ...)  \
             (::poseidon::Async_Logger::is_enabled(level) &&  \
-             ::poseidon::do_xlog_format(level, __FILE__, __LINE__, __func__, "" __VA_ARGS__))
+             ::poseidon::do_xlog_format(level, __FILE__, __LINE__,  \
+                                        __PRETTY_FUNCTION__, "" __VA_ARGS__))
 
 #define POSEIDON_LOG_FATAL(...)   POSEIDON_XLOG_(::poseidon::Async_Logger::level_fatal,  __VA_ARGS__)
 #define POSEIDON_LOG_ERROR(...)   POSEIDON_XLOG_(::poseidon::Async_Logger::level_error,  __VA_ARGS__)
