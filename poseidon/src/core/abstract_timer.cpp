c6b93425c80dd23a89ca6e3f6fe467c4900f1391
Liu Hao <lh_mouse@126.com>
2020-06-22 01:42:19 +0800

    core/abstract_timer: Prevent `period` from being visible before `first` is applied

--- poseidon/src/core/abstract_timer.cpp
+++ poseidon/src/core/abstract_timer.cpp
@@ -18,6 +18,11 @@ Abstract_Timer::
 reset(int64_t first, int64_t period)
 noexcept
   {
+    // Prevent the new period from being visible if the timer is triggered
+    // before it is invalidated, without causing it to be deleted.
+    // This is NOT unnecessary.
+    this->m_period.store(UINT32_MAX);
+
     // Update data members.
     this->m_first.store(first);
     this->m_period.store(period);
