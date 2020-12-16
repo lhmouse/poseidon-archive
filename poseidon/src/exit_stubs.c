252e4f50cb1114e765d74cc08a4d819740bbf4f6
Liu Hao <lh_mouse@126.com>
2020-12-16 14:36:13 +0800

    exit_stubs: Add stub for `__cxa_atexit`

--- poseidon/src/exit_stubs.c
+++ poseidon/src/exit_stubs.c
@@ -1,28 +1,44 @@
 // This file is part of Poseidon.
 // Copyleft 2020, LH_Mouse. All wrongs reserved.
 
-#include <stdlib.h>
 #include <stdio.h>
+
+void
+exit(int status);
+
+int
+atexit(void func(void));
+
+int
+__cxa_atexit(void func(void*), void* arg, void* dso_handle);
 
 void
 exit(int status)
   {
+    // Exit immediately without any cleanups.
     fputs(
-      "WARNING: Please call `quick_exit()` to terminate the process.\n"
-      "         Thorough cleanup is not feasible. We have to call `_Exit()` here.\n",
-      stderr);
+        "WARNING: Please call `quick_exit()` to terminate the process.\n"
+        "         Thorough cleanup is not feasible.\n",
+        stderr);
 
-    // We cannot call `quick_exit()`, which would violate the semantics
-    // of `exit()`. However we cannot invoke callbacks that have been registered
-    // by `atexit()` either, so just exit.
     fflush(0);
     _Exit(status);
   }
 
 int
-atexit(void (*func)(void))
+atexit(void func(void))
   {
     // Pretend that it has been registered successfully.
     (void)func;
+    return 0;
+  }
+
+int
+__cxa_atexit(void func(void*), void* arg, void* dso_handle)
+  {
+    // Pretend that it has been registered successfully.
+    (void)func;
+    (void)arg;
+    (void)dso_handle;
     return 0;
   }
