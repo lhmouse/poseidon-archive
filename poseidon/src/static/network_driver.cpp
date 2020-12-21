94ecec976fad9d2727108033e7ebc5c7ed6cf47a
Liu Hao <lh_mouse@126.com>
2020-12-21 20:47:10 +0800

    static/network_driver: Cleanup

--- poseidon/src/static/network_driver.cpp
+++ poseidon/src/static/network_driver.cpp
@@ -198,13 +198,11 @@ POSEIDON_STATIC_CLASS_DEFINE(Network_Driver)
       {
         // Perform fast lookup using the hint value.
         uint32_t index = self->index_from_epoll_data(epoll_data);
-        if(ROCKET_EXPECT((index < self->m_poll_elems.size())
-                         && (self->m_poll_elems[index].sock->m_epoll_data == epoll_data))) {
-          // Hint valid.
-          ROCKET_ASSERT(index != poll_index_nil);
-          return index;
+        if(ROCKET_EXPECT(index < self->m_poll_elems.size())) {
+          const auto& elem = self->m_poll_elems[index];
+          if(ROCKET_EXPECT(elem.sock->m_epoll_data == epoll_data))
+            return index;
         }
-        POSEIDON_LOG_DEBUG("Epoll lookup hint invalidated: value = $1", epoll_data);
 
         // Perform a brute-force search.
         for(index = 0;  index < self->m_poll_elems.size();  ++index) {
@@ -212,12 +210,12 @@ POSEIDON_STATIC_CLASS_DEFINE(Network_Driver)
           if(elem.sock->m_epoll_data != epoll_data)
             continue;
 
-          // Update lookup hint. Errors are ignored.
+          // Update the lookup hint. Errors are ignored.
           ::epoll_event event;
           event.data.u64 = self->make_epoll_data(index, epoll_data);
           event.events = EPOLLIN | EPOLLOUT | EPOLLET;
           if(::epoll_ctl(self->m_epoll_fd, EPOLL_CTL_MOD, elem.sock->get_fd(), &event) != 0)
-            POSEIDON_LOG_FATAL("failed to modify socket in epoll\n"
+            POSEIDON_LOG_FATAL("Failed to modify socket in epoll\n"
                                "[`epoll_ctl()` failed: $1]",
                                format_errno(errno));
 
@@ -229,7 +227,8 @@ POSEIDON_STATIC_CLASS_DEFINE(Network_Driver)
           return index;
         }
 
-        POSEIDON_LOG_ERROR("Socket not found: epoll_data = $1", epoll_data);
+        // This should not happen at all.
+        POSEIDON_LOG_FATAL("Socket not found: epoll_data = $1", epoll_data);
         return poll_index_nil;
       }
 
