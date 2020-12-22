05f33644ff2c376a05528a1816b9affb7ea6bef3
Liu Hao <lh_mouse@126.com>
2020-12-22 20:47:35 +0800

    main: Update some messages

--- poseidon/src/main.cpp
+++ poseidon/src/main.cpp
@@ -235,12 +235,12 @@ do_check_euid()
       return;
 
     if(::geteuid() == 0)
-      POSEIDON_THROW(
+      POSEIDON_LOG_ERROR(
           "Please do not start this program as root.\n"
-          "If you insist, you may set `general.permit_root_startup` in `main.conf`\n"
-          "to `true` to bypass this check. Note that starting as root is considered\n"
+          "If you insist, you may set `general.permit_root_startup` in `main.conf` "
+          "to `true` to bypass this check. Note that starting as root is considered "
           "insecure. An unprivileged user should have been created for this service.\n"
-          "You have been warned.\n");
+          "You have been warned.");
   }
 
 ROCKET_NOINLINE
@@ -338,8 +338,8 @@ do_check_ulimits()
 
     if((::getrlimit(RLIMIT_NOFILE, &rlim) == 0) && (rlim.rlim_cur <= 10'000))
       POSEIDON_LOG_WARN(
-          "The limit of number of open files (which is `$1`) is too low. This might\n"
-          "result in denial of service when there are too many simultaneous network\n"
+          "The limit of number of open files (which is `$1`) is too low. This might "
+          "result in denial of service when there are too many simultaneous network "
           "connections. We suggest you set it to least 10,000 for production use.\n"
           "See `/etc/security/limits.conf` for details.",
           rlim.rlim_cur);
