f50bdceefc14ca4c2ce0b97e101dbf6852987597
LIU Hao <lh_mouse@126.com>
2022-03-14 17:55:23 +0800

    build: Update multiarch settings

--- makedeb.sh
+++ makedeb.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -e
 
 _pkgname=poseidon
-_pkgversion=$(git describe 2>/dev/null || printf "0.%u.%s" "$(git rev-list --count HEAD)" "$(git rev-parse --short HEAD)")
+_pkgversion=$(printf "0.%u.%s" "$(git rev-list --count HEAD)" "$(git rev-parse --short HEAD)")
+_pkgarch=$(dpkg --print-architecture)
+
 _tempdir=$(readlink -f "./.makedeb")
 _debiandir=${_tempdir}/DEBIAN
 
@@ -14,5 +16,6 @@ cp -p poseidon/etc/main.default.conf ${_tempdir}/etc/poseidon/main.conf
 make install DESTDIR=${_tempdir}
 sed -i "s/{_pkgname}/${_pkgname}/" ${_debiandir}/control
 sed -i "s/{_pkgversion}/${_pkgversion}/" ${_debiandir}/control
+sed -i "s/{_pkgarch}/${_pkgarch}/" ${_debiandir}/control
 
-dpkg-deb --root-owner-group --build .makedeb "${_pkgname}_${_pkgversion}.deb"
+dpkg-deb --root-owner-group --build .makedeb "${_pkgname}_${_pkgversion}_${_pkgarch}.deb"
