47540cc4f1b01f345416acdf5f8d81a62efaf76c
LIU Hao <lh_mouse@126.com>
2022-04-23 21:27:41 +0800

    build: Update 'makedeb.sh'

--- makedeb.sh
+++ makedeb.sh
@@ -11,7 +11,7 @@ rm -rf ${_tempdir}
 mkdir -p ${_tempdir}/etc/poseidon
 cp -pr DEBIAN -T ${_tempdir}/DEBIAN
 cp -pr etc -T ${_tempdir}/etc/poseidon
-cp -p etc/main.default.conf ${_tempdir}/etc/poseidon/main.conf
+cp -p etc/poseidon/main.default.conf ${_tempdir}/etc/poseidon/main.conf
 
 make install DESTDIR=${_tempdir}
 sed -i "s/{_pkgname}/${_pkgname}/" ${_debiandir}/control
