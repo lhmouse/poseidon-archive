2695d88d40104258a32fad4e81a564d05ce36ac9
LIU Hao <lh_mouse@126.com>
2022-06-12 04:07:47 +0800

    main: Minor

--- src/main.cpp
+++ src/main.cpp
@@ -108,7 +108,7 @@ do_exit_printf(Exit_Code code, const char* fmt, ...) noexcept
 
     // Perform fast exit.
     ::fflush(nullptr);
-    ::quick_exit(static_cast<int>(code));
+    ::quick_exit((int)code);
   }
 
 ROCKET_NEVER_INLINE
@@ -294,9 +294,8 @@ do_check_ulimits()
     if((::getrlimit(RLIMIT_NOFILE, &rlim) == 0) && (rlim.rlim_cur <= 10'000))
       POSEIDON_LOG_WARN((
           "The limit of number of open files (which is `$1`) is too low.",
-          "This might result in denial of service when there are too many "
-          "simultaneous network connections. We suggest you set it to least "
-          "10,000 for production use.",
+          "This might result in denial of service when there are too many simultaneous "
+          "network connections. We suggest you set it to least 10,000 for production use.",
           "See `/etc/security/limits.conf` for details."),
           rlim.rlim_cur);
   }
